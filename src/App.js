21f7c4cbd49cb6717022a407356139e6b32141ce
cda3 <massot.etiennepro@gmail.com>
2021-11-05 14:31:06 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './assets/css/App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Actus from './pages/Actus';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <BrowserRouter>
+  <Routes>
+    <Route path='/' exact element={<Home />}/>
+    <Route path='/actus' exact element={<Actus />}/>
+  </Routes>
+  </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from '../components/Navigation';
+
+function Home() {
+    return (
+        <div>
+          <Navigation/>
+            <h1>Welcome Actus</h1>
+        </div>
+    )
+}
+
+export default Home;
