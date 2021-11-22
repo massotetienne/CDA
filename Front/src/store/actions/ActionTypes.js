f09713c4c9976f617f0aa252253613a0cedce0b7
massotetienne <massot.etiennepro@gmail.com>
2021-11-22 15:33:08 +0100

    init

--- Front/src/store/actions/ActionTypes.js
+++
@@ -1,7 +0,0 @@
-//actions type 
-// fonction Get,Put,Post,Delete
-export const GET_METEO_DAY = "GET_METEO_DAY";
-
-export const GET_COUNTRY = 'GET_COUNTRY';
-
-export const GET_ARTICLE = 'GET_ARTICLE';
