9420eba77464fe1f1f5688f3b39276a2fc594baa
massotetienne <massot.etiennepro@gmail.com>
2021-11-24 15:56:46 +0100

    delete

--- Back/Controllers/routeur.js
+++ Back/Controllers/routeur.js
@@ -2,7 +2,7 @@ const
   express = require('express'),
   router = express.Router()
 
-// Import Controller messages
+// Import Controller article
 const ArticleController = require ('./Controller/ArticleController')
 router.route('/article/get')
     .get(ArticleController.getArticle)
